782206f646cd72fda4a328d4a8b947b2cb3ea031
gness1804 <graham.nessler@gmail.com>
2020-08-13 19:38:48 -0500

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Main} from "./Main";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weathrly</h1>
+            <Main cityText="Enter your City"/>
+        <p>Built just in time...</p>
       </header>
     </div>
   );
